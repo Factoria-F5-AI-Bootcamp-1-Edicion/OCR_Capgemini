1f305da84aba590aeee320294608f6de1d4f46fa
RaulMuM <rodriguezppp@gmail.com>
2023-03-29 21:46:26 +0200

    json_parsing.py para crear el archivo json resultado del test y json_api.py para busquedas agrupadas en la API

--- Prueba/json_parsing.py
+++ Prueba/json_parsing.py
@@ -16,11 +16,35 @@ def recursive_search(d, k):
                 return x
 
 def extraer_content(campo_buscado):
-    return campo_buscado['content']
+    if campo_buscado.get("content") is not None: 
+        return campo_buscado['content']
+    else:
+        return ""
+
+# Crear un diccionario para guardar los diccionarios con los datos
+lista_diccionario = {}
+
+for field in datos['fields']:
+    campo_buscado = recursive_search(datos, field)      #Recorre lista de campos
+    #Imprime contenido del nombre de una etiqueta
+    cadena = extraer_content(campo_buscado)
+    #print(cadena)
+    #print("---------------------------------------------------------------------")
+
+    # Dividir el texto en líneas
+    lineas = cadena.split("\n")
+    diccionario = {}
+    for linea in lineas:
+        partes = linea.split(":")
+        if len(partes) == 2:
+            clave, valor = partes
+            diccionario[clave.strip()] = valor.strip()
+
+    lista_diccionario[field] = diccionario
+    # Escribir la lista de diccionarios en un archivo JSON
+    with open("lista_diccionario.json", "w") as archivo_json:
+        json.dump(lista_diccionario, archivo_json)
 
 
-campo_buscado = recursive_search(datos, "ConductorA")
-imprime_contenido = extraer_content(campo_buscado)
-
-
-print(imprime_contenido)
+    # Mostrar el diccionario resultante
+    #print(diccionario)
