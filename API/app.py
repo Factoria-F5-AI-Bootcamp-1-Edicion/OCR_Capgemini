09b758efd8bb348e8e00e6ae67f6ff686d3c3aeb
Andrea Sandoval <andrea.054.1999@gmail.com>
2023-03-31 10:47:31 +0200

    Version 3.0: Se añade ruta de descarga y se empieza a testear

--- API/app.py
+++ API/app.py
@@ -1,8 +1,10 @@
 #Importación de librerías
+import json
 import os
+import tempfile
 import jsonlint
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify, render_template, redirect, json
+from flask import Flask, request, render_template, make_response, send_file
 from azure.ai.formrecognizer import DocumentModelAdministrationClient
 from azure.core.credentials import AzureKeyCredential
 from werkzeug.utils import secure_filename
@@ -25,7 +27,7 @@ app = Flask(__name__)
 # Definir la ruta de la carpeta de subida
 app.config['UPLOAD_FOLDER'] = '/sysroot/home/andreasandoval/Documentos/BOOTCAMP_F5/OCR/OCR_Capgemini/API/Images'
 
-#Se define la ruta de inicio
+#Definir la ruta de inicio
 @app.route('/')
 def hello():
     return render_template("main.html")
@@ -63,18 +65,60 @@ def subir_imagen():
                 print("......found field of type '{}' with value '{}' and with confidence {}".format(field.value_type,
                                                                                                      field_value,
                                                                                                      field.confidence))
-        # Crear un diccionario con los datos que quieres incluir en el JSON
-        json_data = {
-            "doc_type": document.doc_type,
-            "confidence": document.confidence,
-            "fields": {name: field.value if field.value else field.content for name, field in document.fields.items()}
-        }
 
-        # Convertir el diccionario en un objeto JSON con formato legible
-        pretty_json = json.dumps(json_data, indent=4)
+        n = 0;
+        diccionario = {}
 
-        # Enviar el JSON como respuesta
-        return jsonify(pretty_json)
+        for document in result.documents:
+            n += 1
+            for name, field in document.fields.items():
+                field_value = field.value if field.value else field.content
+                # convertir el valor a una cadena si no es una cadena
+                if not isinstance(field_value, str):
+                    field_value = str(field_value)
+                diccionario[name] = field_value
 
-if __name__ == "__main__":
-    app.run()
+        try:
+            data = jsonlint.ValidationError(diccionario)
+            print("\n El JSON es válido/reparado:", data)
+
+        except json.JSONDecodeError as err:
+            print("\n El JSON no es válido:" + str(err))
+
+            # print(data)
+
+        print("----------------------------------------")
+
+        # Serializar el objeto a JSON con opciones personalizadas
+        json_str = json.dumps(diccionario, ensure_ascii=False, sort_keys=True, indent=4)
+
+        # Renderizar la plantilla * con el JSON serializado
+        return render_template('result.html', json_data=json_str)
+
+
+# Se define la ruta para descargar el archivo JSON generado
+@app.route('/download', methods=['POST'])
+def download():
+    json_str = request.form['json_str']
+
+    # Crear un archivo temporal para almacenar el JSON
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.json') as tmp:
+        tmp.write(json_str.encode('utf-8'))
+        nombre_archivo, _ = os.path.splitext(os.path.basename(tmp.name))
+        nombre_archivo_json = nombre_archivo + ".json"
+
+    # Crear una respuesta para enviar el archivo
+    response = make_response(send_file(tmp.name, as_attachment=True))
+
+    # Establecer las cabeceras para forzar la descarga del archivo
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Content-Disposition'] = f'attachment; filename={nombre_archivo_json}'
+
+    return response
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5001)
--- API/test_app.py
+++ API/test_app.py
@@ -1,35 +1,42 @@
+import json
 import os
-from io import BytesIO
-from pytest_mock import mocker
-from flask import Flask
-from werkzeug.datastructures import FileStorage
-from app import subir_imagen
+import tempfile
+import pytest
+from app import app
+from bs4 import BeautifulSoup
 
-def test_subir_imagen(mocker):
-    # Configurar la aplicación Flask
-    app = Flask(__name__)
-    app.config['UPLOAD_FOLDER'] = 'tests/images'
+def test_hello():
+    client = app.test_client()
 
-    # Cargar una imagen de prueba
-    image_path = os.path.join(os.path.dirname(__file__), 'images', 'test_image.png')
-    with open(image_path, 'rb') as f:
-        file = FileStorage(BytesIO(f.read()), filename='test_image.png')
+    # Simular una solicitud GET a la ruta "/"
+    response = client.get('/')
 
-    # Configurar el objeto mocker para la llamada a Azure Form Recognizer
-    mock_poller = mocker.Mock()
-    mock_poller.result.return_value = mock_result()
-    mock_client = mocker.Mock()
-    mock_client.begin_analyze_document.return_value = mock_poller
-    mocker.patch('app.DocumentAnalysisClient', return_value=mock_client)
-
-    # Ejecutar la función subir_imagen() con la imagen de prueba
-    with app.test_request_context('/analize_image', method='POST', data={'userfile': file}):
-        response = subir_imagen()
-
-    # Verificar que se haya devuelto un objeto JSON con los datos esperados
+    # Verificar que la respuesta tenga un status code 200 OK
     assert response.status_code == 200
-    json_data = response.get_json()
-    assert 'doc_type' in json_data
-    assert 'confidence' in json_data
-    assert 'fields' in json_data
-    assert len(json_data['fields']) > 0
+
+    # Verificar que la plantilla "main.html" se renderice correctamente
+    assert b'<!DOCTYPE html>' in response.data
+
+
+def test_subir_imagen():
+    client = app.test_client()
+
+    # Cargar la imagen de prueba
+    image_path = os.path.join(os.path.dirname(__file__), 'test_img.jpg')
+
+    # Simular una solicitud POST a la ruta "/analize_image" con el archivo de prueba como datos adjuntos
+    with open(image_path, 'rb') as f:
+        response = client.post('/analize_image', data={'userfile': (f, 'test_img.jpg')}, content_type='multipart/form-data')
+
+    # Verificar que la respuesta tenga un status code 200 OK
+    assert response.status_code == 200
+
+    # Verificar que la plantilla "result.html" se renderice correctamente
+    soup = BeautifulSoup(response.data, 'html.parser')
+    assert soup.find('div', {'id': 'json_data'}) is not None
+
+    # Obtener el JSON serializado del HTML
+    json_str = soup.find('div', {'id': 'json_data'}).text
+
+    # Cargar el JSON
+    data = json.loads(json_str)
