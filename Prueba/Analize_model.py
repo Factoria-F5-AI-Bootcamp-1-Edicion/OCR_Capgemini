47558c17989618033565447511cfebd9519e8f16
RaulMuM <rodriguezppp@gmail.com>
2023-04-04 00:08:01 +0200

    Modificando la function_json_parsing para que tome todas las etiquetas que faltaban por mostrar

--- Prueba/Analize_model.py
+++ Prueba/Analize_model.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 endpoint = os.getenv("AZURE_FORM_RECOGNIZER_ENDPOINT")
 key = os.getenv("AZURE_FORM_RECOGNIZER_KEY")
-model_id = "model_1"
+model_id = "Model_2"
 formUrl = "https://github.com/aratan/Azure-OCR/blob/adfe9d0db7be28cda624e9ea1370d84a67779627/parte_amistoso_0.jpg?raw=true"
 
 document_analysis_client = DocumentAnalysisClient(endpoint=endpoint, credential=AzureKeyCredential(key))
@@ -21,7 +21,7 @@ document_analysis_client = DocumentAnalysisClient(endpoint=endpoint, credential=
 # Make sure your document's type is included in the list of document types the custom model can analyze
 poller = document_analysis_client.begin_analyze_document_from_url(model_id, formUrl)
 result = poller.result()
-
+'''
 for idx, document in enumerate(result.documents):
     print("--------Analyzing document #{}--------".format(idx + 1))
     print("Document has type {}".format(document.doc_type))
@@ -32,7 +32,7 @@ for idx, document in enumerate(result.documents):
         print("......found field of type '{}' with value '{}' and with confidence {}".format(field.value_type,
                                                                                              field_value,
                                                                                              field.confidence))
-
+'''
 n = 0;
 diccionario = {}
 
@@ -49,6 +49,7 @@ for document in result.documents:
 try:
     data = jsonlint.ValidationError(diccionario)
     print("\n El JSON es válido/reparado:", data)
+
 
 except json.JSONDecodeError as err:
     print("\n El JSON no es válido:" + str(err))
--- Prueba/json_parsing_sdk.py
+++ Prueba/json_parsing_sdk.py
@@ -3,9 +3,11 @@ import re
 
 
 def function_load_json(archive_json):
+    datos = None
     with open(archive_json, 'r') as archivo:
         # Cargar el contenido del archivo JSON en una variable
         datos = json.load(archivo)
+    return datos
 
 
 def recursive_search(d, k):
@@ -25,40 +27,82 @@ def extraer_content(campo_buscado):
         return ""
 
 
+
+def guardar_datos(dic_original):
+    dic_modificado = {}
+    for clave, valor in dic_original.items():
+        if isinstance(valor, dict):
+            dic_modificado[clave] = guardar_datos(valor)
+        elif isinstance(valor, str):
+            if '\n' in valor and clave not in ["Hora", "Fecha", "Lugar", "Testigos", "VehiculoA", "VehiculoB"]:
+                # Definir patrón
+                patron = r'\s+(?=(?:Apell\w+|Direc\w+|C[oó]di\w+|N.º\s\w+|\bPa[ií]s\b(?:[\s:]\w+)?|Fecha\s\w+))'  # Espacio en blanco delante y despues la exprexion a buscar
+                dicc = {}
+                # Dividir la cadena según el patrón
+                lineas_patron = re.sub(patron, r'\n\g<0>', valor)
+                lineas = lineas_patron.split("\n")
+                for linea in lineas:
+                    #print(linea)
+                    partes = linea.split(":")
+                    if len(partes) == 2:
+                        k, v = partes
+                        dicc[k.strip()] = v.strip()
+                    elif len(partes) == 3:
+                        dicc[partes[0].strip()] = partes[1].strip()
+                dic_modificado[clave] = dicc
+            else:
+                if clave not in ["VehiculoA", "VehiculoB"]:
+                    dic_modificado[clave] = valor.split('\n')[0]
+                else:
+                    dic_modificado[clave] = valor
+    return dic_modificado
+
+
 def function_json_parsing(datos):
     # Abrir el archivo JSON en modo de lectura
     if (datos == None):
-        function_load_json("result_1_test.json")
+        datos = function_load_json("N°casillas_B.json")
 
+    #print(datos)
     # Crear un diccionario para guardar los diccionarios con los datos
     lista_diccionario = {}
     data = {}
 
+    lista_diccionario = guardar_datos(datos)
+    '''
     for k, v in datos.items():
 
         # Dividir el texto en líneas
-
+        #print(k)
+        #print(v)
         diccionario = {}
+        
+        if '\n' not in v:
+            lista_diccionario[k]=v
 
-        # Definir patrón
-        patron = r'\s+(?=(?:Apell\w+|Direc\w+|C[oó]di\w+|N.º\s\w+|\bPa[ií]s\b(?:[\s:]\w+)?|Fecha\s\w+))'  # Espacio en blanco delante y despues la exprexion a buscar
+        else:
+            # Definir patrón
+            patron = r'\s+(?=(?:Apell\w+|Direc\w+|C[oó]di\w+|N.º\s\w+|\bPa[ií]s\b(?:[\s:]\w+)?|Fecha\s\w+))'  # Espacio en blanco delante y despues la exprexion a buscar
 
-        # Dividir la cadena según el patrón
-        lineas_patron = re.sub(patron, r'\n\g<0>', v)
-        lineas = lineas_patron.split("\n")
-        print(lineas)
-        for linea in lineas:
-            partes = linea.split(":")
-            if len(partes) == 2:
-                clave, valor = partes
-                diccionario[clave.strip()] = valor.strip()
-            elif len(partes) == 3:
-                diccionario[partes[0].strip()] = partes[1].strip()
+            # Dividir la cadena según el patrón
+            lineas_patron = re.sub(patron, r'\n\g<0>', v)
+            lineas = lineas_patron.split("\n")
+            #print(lineas)
+            for linea in lineas:
+                partes = linea.split(":")
+                if len(partes) == 2:
+                    clave, valor = partes
+                    diccionario[clave.strip()] = valor.strip()
+                elif len(partes) == 3:
+                    diccionario[partes[0].strip()] = partes[1].strip()
 
         lista_diccionario[k] = diccionario
-        data= lista_diccionario
+    '''
+    #print(lista_diccionario)
         # Escribir la lista de diccionarios en un archivo JSON
-        with open("result_test_sdk_2.json", "w") as archivo_json:
-            json.dump(lista_diccionario, archivo_json, ensure_ascii=False, sort_keys=True, indent=4 )
+    with open("result_test_sdk_2.json", "w") as archivo_json:
+        json.dump(lista_diccionario, archivo_json, ensure_ascii=False, sort_keys=True, indent=4)
 
-    return data
+    return lista_diccionario
+
+#function_json_parsing(None)
